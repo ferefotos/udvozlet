01074eac638b4af00a3c89ccb3266501fe17dbdb
ferefotos <reg@ferefoto.hu>
2018-10-07 10:51:34 +0200

    Első változat. Egy osztály

---
+++ src/udvozlet/Udvozlet.java
@@ -0,0 +1,17 @@
+package udvozlet;
+
+
+/**
+ *
+ * @author Fere
+ */
+public class Udvozlet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Üdvözöllek!");
+    }
+    
+}
